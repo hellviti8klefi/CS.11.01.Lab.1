857e130812dc09037e916c6e4915d26bc70b9e17
hellviti8klefi <ruijsmiya@gmail.com>
2024-08-23 12:40:28 +0800

    Lab 1 finished

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter your age");
+      int age = sc.nextInt();
+
+      int youngestDA = (age/2)+7;
+
+      System.out.println("The youngest dating age is" + youngestDA);
+
+
+
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
+
+
+        int readpages = (100-age);
+
+        System.out.println("You should read " + readpages);
 
     }
 }
